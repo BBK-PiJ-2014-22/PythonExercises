ef6128624bd3f1c328770142f82642d6f13bf9dc
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-02-12 17:50:13 +0000

    Merge v02 created
    Splits into multiple files

---
+++ csvmergev0_2.py
@@ -0,0 +1,31 @@
+import csv
+import os
+
+directory = "C:\\Users\Jamie\Downloads\Companies House Data"
+files = os.listdir(directory)
+
+
+#Assign the headings
+
+filepath = directory + '\\' + files[0]
+newfilepath = directory  + '\combined.csv'
+
+with open(filepath, 'r', newline = '', encoding="utf8") as csvfile:
+    with open(newfilepath,'w', newline = '') as newfile:
+        reader = csv.reader(csvfile)
+        writer = csv.writer(newfile)
+        writer.writerow(next(reader))
+
+
+#Combine files
+for file in files:
+    filepath = directory + '\\' + 'BasicCompanyData-2015-02-01-part4_5.csv'
+    with open(filepath, 'r', newline = '', encoding="utf8") as oldfile:
+        with open(newfilepath, 'a', newline = '') as newfile:
+            reader = csv.reader(oldfile, delimiter= ",")
+            writer = csv.writer(newfile, delimiter= ",")
+            next(reader) #skips heading
+            data = [row for row in reader]
+            writer.writerows(data)
+
+print('done')
